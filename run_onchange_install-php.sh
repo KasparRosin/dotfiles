9b715fa5d724e15bbbdf0209922dc37422228088
Kaspar Rosin <kaspar@rosin.dev>
2026-01-21 20:02:12 +0200

    feat: add php installation

---
+++ run_onchange_install-php.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing PHP 8.4 with common extensions..."
+sudo apt update && sudo apt install -y \
+    php8.4-cli \
+    php8.4-common \
+    php8.4-curl \
+    php8.4-mbstring \
+    php8.4-xml \
+    php8.4-zip \
+    php8.4-intl \
+    php8.4-bcmath \
+    php8.4-gd \
+    php8.4-mysql \
+    php8.4-opcache
+
+echo "Installing Composer..."
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+echo "PHP and Composer installation completed!"
+php --version
+composer --version
